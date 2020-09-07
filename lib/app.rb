cbc2dc896cd038843903cabd9ce2bcef7376fc79
Alexander Shagov <alexander.shgv@gmail.com>
2020-09-07 19:39:55 +0300

    Download images in threads

--- lib/app.rb
+++ lib/app.rb
@@ -1,5 +1,16 @@
+require_relative 'models/file.rb'
+require_relative 'models/image.rb'
+
 class App
   def self.run(file_path:, download_path:)
     file = Models::File.new(file_path)
+
+    threads = []
+    file.entries.each do |image_url|
+      threads << Thread.new(image_url) do
+        Models::Image.new(image_url).download(to: download_path)
+      end
+    end
+    threads.each(&:join)
   end
 end
